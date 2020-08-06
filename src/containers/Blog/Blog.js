aa6906021dab6a48d5198a7fe018557234b0585e
Kyle Weidner <kyle.weidner14@protonmail.com>
2020-08-06 17:24:50 -0600

    set up router package and prepare for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,56 +2,17 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts'
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false
-  }
 
-  componentDidMount() {
-    axios.get('/posts') // sends GET req to url
-        .then(response => {
-          const posts = response.data.slice(0,4); //stores first 4 posts from JSON into 'const posts'
-          const updatedPosts = posts.map(post => {
-            return {
-              ...post,
-              author: 'Kyle'
-            } // takes all posts and adds 'author' prop
-          });
-          this.setState({
-            posts: updatedPosts
-          })
-        })
-        .catch(error => {
-          // console.log(error);
-          this.setState({error: true})
-        });
-  }
 
-  postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id})
-  }
+
+
+
 
   render () {
-    let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
-    if (!this.state.error) {
-      posts = this.state.posts.map( post => { // maps posts from state into new array 'posts'
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)} //passes 'id' from Post to postSelectedHandler(id)
-            /> //post.title comes from the data (has title property in JSON)
-          }
-      );
-    }
-
     return (
       <div className="Blog">
         <header>
@@ -62,16 +23,9 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className="Posts">
-          {/*output array of posts*/}
-          {posts}
-        </section>
-        <section>
-            <FullPost id={this.state.selectedPostId}/>
-        </section>
-        <section>
-            <NewPost />
-        </section>
+
+        <Posts/>
+
       </div>
     );
   }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import axios from "../../../axios";
+
+import Post from '../../../components/Post/Post'
+import './Posts.css'
+
+
+class Posts extends Component {
+  state = {
+    posts: []
+  }
+  postSelectedHandler = (id) => {
+    this.setState({selectedPostId: id})
+  }
+
+  componentDidMount() {
+    axios.get('/posts') // sends GET req to url
+        .then(response => {
+          const posts = response.data.slice(0,4); //stores first 4 posts from JSON into 'const posts'
+          const updatedPosts = posts.map(post => {
+            return {
+              ...post,
+              author: 'Kyle'
+            } // takes all posts and adds 'author' prop
+          });
+          this.setState({
+            posts: updatedPosts
+          })
+        })
+        .catch(error => {
+          console.log(error);
+          //this.setState({error: true})
+        });
+  }
+
+  render () {
+    let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
+    if (!this.state.error) {
+      posts = this.state.posts.map( post => { // maps posts from state into new array 'posts'
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)} //passes 'id' from Post to postSelectedHandler(id)
+            /> //post.title comes from the data (has title property in JSON)
+          }
+      );
+    }
+
+    return (
+        <section className="Posts">
+          {posts}
+        </section>
+    )
+  }
+}
+
+export default Posts;
